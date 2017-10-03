bb337d54e5018cab831421353122d2c6f939cca0
Sathish Duraisamy <sduraisa@gmail.com>
2017-10-02 23:30:21 -0700

    Minor cosmetic changes

--- install_python_from_src.sh
+++ install_python_from_src.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+# -- Globals --
 hw_plat=$(uname -p)   # 'i686' or 'x86_64'
 el_ver=$(uname -r | sed -r -e 's/^.*\.el([0-9]+)\..*/\1/')   # '6' from  ..el6..
 el_str="el${el_ver}"
@@ -9,10 +10,12 @@ tmpdir=/tmp/pkgs
 [[ -d "$tmpdir" ]] || mkdir $tmpdir
 
 
+# -- Functions --
 croak() {
   echo $1
   exit 1
 }
+
 
 rpmsearch_and_install() {
   pkg_name=$1
@@ -29,6 +32,7 @@ rpmsearch_and_install() {
       && echo "(done)"
 }
 
+
 yum_install_pkg() {
   pkg_name=$1
   echo -n "|-->  Installing pkg '$pkg_name' ..."
@@ -39,6 +43,7 @@ yum_install_pkg() {
   fi
 }
 
+
 install_python_from_source() {
     py_ver=$1
     [[ -z "$py_ver" ]] && croak "Specify python version"
@@ -46,13 +51,10 @@ install_python_from_source() {
     tgt_dir="/usr/local/pythons/python${py_ver}.${el_str}.${hw_plat}"  # Ex pythons/python2.6.6.el6.i686
     command -v $tgt_dir/bin/python && return
 
-    # Pyhapi needs python3. We can stick to Python3.5.3
-    # if installing fresh, lets isntall these before
-
     # In general, before installing Python3, these are recommended
     # During Python3 install, 'ensurepip' may fail if openssl-devel is absent
     # “Ignoring ensurepip failure: pip 8.1.1 requires SSL/TLS”
-    # Pyhapi needs pymodule 'readline' which needs 'libncurses'
+    # Requiring pymodule 'readline' would in turn require 'libncurses'
     # /bin/ld: cannot find -lncurses
     #  # Note: couldn't find rpm for 'tcllib' anywhere
     for pkg in openssl openssl-devel sqlite sqlite-devel zlib zlib-devel bzip2 bzip2-devel \
@@ -67,10 +69,13 @@ install_python_from_source() {
         python_src_file=Python-${py_ver}
         curl -O "https://www.python.org/ftp/python/${py_ver}/$python_src_file.tgz"
         tar xvfz $python_src_file.tgz && cd $python_src_file   \
-            && sudo ./configure --enable-shared --with-ensurepip=install --prefix=$tgt_dir && sudo make && sudo make install
+            && sudo ./configure --enable-shared --with-ensurepip=install \
+                  --prefix=$tgt_dir && sudo make && sudo make install
     fi
 }
 
+
+# ##############################################################################
 
 # install_python_from_source '2.7.13'
 install_python_from_source '2.6.6'
